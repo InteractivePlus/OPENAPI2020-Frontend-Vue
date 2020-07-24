3c03740d58b3eb0964a65d4b16551180875ec404
zHElEARN <zhe_learn@liuzeyun.tech>
2020-07-24 19:18:01 +0800

    modify the registration page

--- src/components/register.vue
+++ src/components/register.vue
@@ -1,44 +1,94 @@
 <template>
 	<div class="container_wrap">
-		<xsydContainer title="注册" subTitle="一个账号，畅享BlueAirLive所有服务">
+		<xsydContainer title="注册您的形随意动账号" islogohidden="true" isTitleCenter="false">
 			<div>
-				<div>
-					<xsydInput inputId="EmailInput" label="邮箱" placeholderStr="请输入您的邮箱"></xsydInput>
-					<xsydInput inputId="PwdInput" label="密码" placeholderStr="请输入密码" isPwd="true"></xsydInput>
-					<a-button v-on:click="goRegister" type="primary" name="next-step" size="large">注册</a-button>
-				</div>
-				<div class="form_tips">
-					<router-link to="/login">
-					<div class="pull-left"><a href="" class="login-link">登录账号</a></div>
-					</router-link>
-					<div class="pull-right"><a href="" class="login-link">服务条约</a></div>
-				</div>
+				<a-row type="flex" justify="space-around" align="middle">
+					<a-col :span="16">
+						<div>
+							<xsydInput
+								label="昵称"
+								inputId="FirstNameInput"
+								placeholderStr="请输入您的昵称"
+							></xsydInput>
+							<xsydInput
+								label="用户名"
+								inputId="UserNameInput"
+								placeholderStr="请输入您的用户名"
+							></xsydInput>
+							<xsydInput label="邮箱" inputId="EmailInput" placeholderStr="请输入您的邮箱"></xsydInput>
+							<p id="email-tip">您需要证实此电子邮件地址属于您。</p>
+							<xsydInput
+								label="密码"
+								id="password"
+								inputId="PasswordInput"
+								placeholderStr="请输入您的密码"
+							></xsydInput
+							><xsydInput
+								id="confirm-password"
+								inputId="ConfirmPasswordInput"
+								label="确认密码"
+								placeholderStr="请确认您的密码"
+							></xsydInput>
+							<p id="password-tip">使用 8 个或更多字符（字母、数字和符号的组合）</p>
+						</div>
+						<a-row>
+							<a-col :span="12"><a href="">已有账号？</a></a-col>
+							<a-col :span="12">
+								<a-button v-on:click="goRegister" type="primary" name="next-step" size="medium"
+									>下一步</a-button
+								>
+							</a-col>
+						</a-row>
+					</a-col>
+					<a-col :span="8">
+						<a-row>
+							<a-col :span="24"><img src="@/assets/logo.png" class="container-logo"/></a-col>
+							<a-col :span="24"><p id="regist-tip">一个账号，畅享BlueAirLive所有服务</p></a-col>
+						</a-row>
+					</a-col>
+				</a-row>
 			</div>
 		</xsydContainer>
 	</div>
 </template>
 
 <script>
+import xsydContainer from "@/components/widget/xsydContainer.vue";
+import xsydInput from "@/components/widget/xsydInput.vue";
 
-import xsydContainer from '@/components/widget/xsydContainer.vue';
-import xsydInput from '@/components/widget/xsydInput.vue';
 export default {
-	name: 'register',
+	name: "register",
 	components: {
 		//testui
 		xsydContainer,
-		xsydInput
+		xsydInput,
 	},
 	methods: {
-	    goRegister() {
-	        console.log('register');
-	    }
-	}
+		goRegister() {
+			console.log("register");
+		},
+	},
 };
 </script>
 
 <style>
 #app {
 	height: inherit;
+}
+
+#email-tip {
+	font-size: 12px;
+	text-align: left;
+	margin-bottom: 2em;
+}
+
+#password-tip {
+	font-size: 12px;
+	text-align: left;
+}
+
+#regist-tip {
+	font-size: 16px;
+	font-weight: 300;
 }
 </style>
--- src/components/widget/xsydContainer.vue
+++ src/components/widget/xsydContainer.vue
@@ -1,10 +1,15 @@
 <template>
-	<div class="xsyd-container" >
+	<div :class="islogohidden === 'false' ? 'xsyd-container' : 'xsyd-container-without-logo'">
 		<div v-if="islogohidden === 'false'">
-			<a href=""><img src="@/assets/logo.png" class="container-logo" /></a>
+			<a href=""><img src="@/assets/logo.png" class="container-logo"/></a>
 		</div>
 		<div class="container_desc">
-			<h1 class="container-text">{{ title }}</h1>
+			<div v-if="isTitleCenter === 'true'">
+				<h1 class="container-text">{{ title }}</h1>
+			</div>
+			<div v-if="isTitleCenter === 'false'">
+				<h1 class="container-text-without-center">{{ title }}</h1>
+			</div>
 			<h2 class="container-little-text">{{ subTitle }}</h2>
 		</div>
 		<slot></slot>
@@ -12,22 +17,25 @@
 </template>
 
 <script>
-import '@/assets/css/widget/xsydContainer.css';
+import "@/assets/css/widget/xsydContainer.css";
 
 export default {
 	props: {
 		title: {
 			type: String,
-			required: true
+			required: true,
 		},
 		cwidth: {
-			default: '1000px'
+			default: "1000px",
 		},
 		subTitle: String,
 		islogohidden: {
-			default: 'false'
-		}
-	}
+			default: "false",
+		},
+		isTitleCenter: {
+			default: "true",
+		},
+	},
 };
 </script>
 
